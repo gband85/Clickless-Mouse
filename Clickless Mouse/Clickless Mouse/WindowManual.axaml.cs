0bc013648a62560c67f4e0c207740b62c72b0a7b
Garrett Anderson <gband85@mailfence.com>
2025-01-11 21:07:32 -0600

    add english manual

---
+++ Clickless Mouse/Clickless Mouse/WindowManual.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Clickless_Mouse;
+
+public partial class WindowManual : Window
+{
+    public WindowManual()
+    {
+        InitializeComponent();
+    }
+}
